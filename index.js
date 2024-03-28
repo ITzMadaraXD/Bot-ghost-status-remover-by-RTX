ab68772c6029634745ef4ce16483f73002c8e3a2
ITzMadaraXD <158747848+ITzMadaraXD@users.noreply.github.com>
2024-03-28 12:04:57 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to /","Watching Members];
 
 
 let currentIndex = 0;
