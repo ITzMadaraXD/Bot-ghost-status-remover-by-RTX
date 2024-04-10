f892c8721419462bffe079b0d7a59d6448c80cb2
ITzMadaraXD <158747848+ITzMadaraXD@users.noreply.github.com>
2024-04-10 19:46:26 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to ME","Watching Members"];
+const statusMessages = ["Listening to astral","stores & services"];
 
 
 let currentIndex = 0;
